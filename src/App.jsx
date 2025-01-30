f3a973947c25f64ec66901ce5f330e2636d23590
Your Name <you@example.com>
2025-01-30 12:54:52 -0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import  { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Cadastro from './pages/Cadastro'
+import Login from './pages/Login'
 
 function App() {
-    
+
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Cadastro />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </BrowserRouter>
 
     </div>
@@ -17,6 +20,7 @@ function App() {
 
 
 export default App
+
 // VITE + REACT CONFIGURATIONS MODEL 
 
 
